74fb477dc26a8ef411109df289ede2d556b34025
complex-systems-rg <56405579+complex-systems-rg@users.noreply.github.com>
2019-10-18 16:43:18 +0300

    Add files via upload

---
+++ Iterated Prisoner's Dilemma/Çetin's Article/Meltem/cooperator.py
@@ -0,0 +1,5 @@
+from player import Player
+
+class Cooperator(Player):
+  def __init__(self, M):
+    super().__init__("C", M)
---
+++ Iterated Prisoner's Dilemma/Çetin's Article/Meltem/defector.py
@@ -0,0 +1,5 @@
+from player import Player
+
+class Defector(Player):
+  def __init__(self, M):
+    super().__init__("D", M)
