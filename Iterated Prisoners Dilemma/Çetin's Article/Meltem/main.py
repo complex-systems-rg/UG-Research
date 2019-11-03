fcdf5477e0e753e6bb7f6125ff377ad037b6cde1
Meltem Suiçmez <41024883+Melteos@users.noreply.github.com>
2019-11-03 13:18:54 +0300

    working code

--- Iterated Prisoners Dilemma/Çetin's Article/Meltem/main.py
+++ Iterated Prisoners Dilemma/Çetin's Article/Meltem/main.py
@@ -34,6 +34,7 @@ def decideToPlay(Player1, Player2):
     return True
 
 def GAME(N, M, D, P, R, S, T,ti):
+  playerList = []
   pOfC = 0
   pOfD = 0
   for i in range(D):
@@ -60,17 +61,14 @@ def GAME(N, M, D, P, R, S, T,ti):
   gameStatistics["avg payoff of d"].append(round(pOfD,3))
   gameStatistics["remainder"].append(round((pOfC-pOfD),3))
 
-N = 100
-#M = sys.argv[1]
-#D = sys.argv[2]
-R = 3
-S = 0
-T = 5
-P = 1
-ti = 2
-for i in range(20):
-      GAME(N, 50, 50, P, R, S, T,ti)
+N,R,S,T,P,ti = 100,3,0,5,1,2
+
+for M in range(10, 100, 10):
+  for D in range(10, 100, 10):
+    for i in range(20):
+      GAME(N, M, D, P, R, S, T,ti)
+  print("M is " + str(M))
 
 gameStatistics =  pd.DataFrame.from_dict(gameStatistics)
-pd.DataFrame.to_csv("experiment.csv")
+gameStatistics.to_csv("experiment.csv")
 
